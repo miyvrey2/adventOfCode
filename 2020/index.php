ecc80dd69eb9b0e317d118de60a101c2497d9f00
Jeffrey S <jeffrey_wii@hotmail.com>
2020-12-03 21:29:00 +0100

    test

--- 2020/index.php
+++ 2020/index.php
@@ -10,12 +10,12 @@
             <div class="row">
                 <div class="col-md-12">
                     <h1>Advent of Code</h1>
-                    <a href="01/part1/index.php">01 - part1</a><br>
-                    <a href="01/part2/index.php">01 - part2</a><br>
-                    <a href="02/part1/index.php">02 - part1</a><br>
-                    <a href="02/part2/index.php">02 - part2</a><br>
-                    <a href="03/part1/index.php">03 - part1</a><br>
-                    <a href="03/part2/index.php">03 - part2</a><br>
+                    <a href="01/part1/index.php">01 - part1</a><br />
+                    <a href="01/part2/index.php">01 - part2</a><br />
+                    <a href="02/part1/index.php">02 - part1</a><br />
+                    <a href="02/part2/index.php">02 - part2</a><br />
+                    <a href="03/part1/index.php">03 - part1</a><br />
+                    <a href="03/part2/index.php">03 - part2</a><br />
                 </div>
             </div>
         </div>
