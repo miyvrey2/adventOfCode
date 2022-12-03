1b875704902121480f8e31455f660ec089cb04a0
Jeffrey S <jeffrey@accentinteractive.nl>
2022-12-03 08:09:09 +0100

    Improved results for third day Puzzled the third day for 2022

--- 2022/03/part1/index.php
+++ 2022/03/part1/index.php
@@ -5,8 +5,8 @@ $input = explode(PHP_EOL, file_get_contents('./input.txt', true));
 
 // Retrieve all lines
 $lines = import_lines_as_array($input);
-$errorInput = find_error_input($lines);
-dump($lines);
+$sum = sum_priority_of_item_types($lines);
+dump($sum);
 
 function import_lines_as_array($input)
 {
@@ -21,34 +21,59 @@ function import_lines_as_array($input)
     return $lines;
 }
 
-function find_error_input($lines) {
-    $errorInput = [];
+/**
+ * @param $lines
+ *
+ * @return int
+ */
+function sum_priority_of_item_types($lines): int
+{
     $score = 0;
-    $alphabetLowercase = range('a', 'z');
-    $alphabetUppercase = range('A', 'Z');
-    $alphaToInt = array_merge($alphabetLowercase, $alphabetUppercase);
-    $alphaToInt = array_combine(range(1, count($alphaToInt)), array_values($alphaToInt));
+    $alphaToInt = alphabetToInt();
 
-    foreach ($lines as $key => $line) {
+    foreach ($lines as $line) {
 
+        // Get each (unique) character
         $chars = array_unique(str_split($line[0]));
 
-        foreach($chars as $char) {
-            if(strpos($line[1], $char) !== false) {
-                $errorInput[] = $char;
+        foreach ($chars as $char) {
+
+            if (strpos($line[1], $char) !== false) {
                 $score += array_search($char, $alphaToInt);
             }
         }
     }
 
-    dump_exit($score);
+    return $score;
 }
 
-function dump($var) {
+/**
+ * Returns array where key 1 = a, 2 = b, 3 = c, ... A = 27, B = 28, C = 29
+ *
+ * @return array
+ */
+function alphabetToInt(): array
+{
+    // Make 2 arrays (lower and uppercase)
+    $alphabetLowercase = range('a', 'z');
+    $alphabetUppercase = range('A', 'Z');
+
+    // Merge the array
+    $alphabetToInt = array_merge($alphabetLowercase, $alphabetUppercase);
+
+    // Make sure the key start from 1, and not 0
+    $alphabetToInt = array_combine(range(1, count($alphabetToInt)), array_values($alphabetToInt));
+
+    return $alphabetToInt;
+}
+
+function dump($var)
+{
     echo "<pre style='background-color: #86ae84; color: #283028; padding: 3px; border: 2px dashed #526c51;'>";var_dump($var);echo "</pre>";
 }
 
-function dump_exit($var) {
+function dump_exit($var)
+{
     dump($var);
     exit();
 }
