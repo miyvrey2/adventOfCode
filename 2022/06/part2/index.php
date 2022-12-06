12f5dbab4212aed0e3b9ec797360921097c6a448
Jeffrey S <jeffrey@accentinteractive.nl>
2022-12-06 07:50:21 +0100

    Puzzled the sixth day for 2022

---
+++ 2022/06/part2/index.php
@@ -0,0 +1,42 @@
+<?php
+
+// Get the .txt
+$input = explode(PHP_EOL, file_get_contents('./input.txt', true));
+
+// Retrieve all lines
+$chars = import_lines_as_array($input);
+$score = score($chars);
+
+function import_lines_as_array($input)
+{
+    $lines = [];
+
+    foreach ($input as $line) {
+        $lines[] = $line;
+    }
+    $chars = str_split( $lines[0]);
+
+    return $chars;
+}
+
+function score($chars)
+{
+    foreach($chars as $key => $char) {
+        $tmp_array = array_slice($chars, $key, 14   );
+        $tmp_array = array_unique($tmp_array);
+        if(count($tmp_array) == 14) {
+            dump_exit($key + 14);
+        }
+    }
+}
+
+function dump($var)
+{
+    echo "<pre style='background-color: #86ae84; color: #283028; padding: 3px; border: 2px dashed #526c51;'>";var_dump($var);echo "</pre>";
+}
+
+function dump_exit($var)
+{
+    dump($var);
+    exit();
+}
